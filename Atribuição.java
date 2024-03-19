b875b0012df1695a5be570cd16ec9bbd5f7e60a4
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-03-19 17:11:40 -0300

    Add files via upload
    Uso de "+=" e "-=", e aprendendo a diferença entre "++code" e "code++"

---
+++ Atribuição.java
@@ -0,0 +1,15 @@
+public class Atribuição {
+  public static void main(String[] args){
+    int Bonus = 2000;
+    Bonus += 450;
+    Bonus += 550;
+    Bonus /= 3;
+    System.out.println(Bonus);
+
+    int Conta = 0;
+    Conta++;
+    Conta--;
+    System.out.println(Conta++);
+    System.out.println(Conta);
+  }
+}
