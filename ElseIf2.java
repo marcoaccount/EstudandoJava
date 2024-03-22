e398fdc56e14c4e0e4633a5e80f2996bd261e835
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-03-22 20:34:26 -0300

    Add files via upload

---
+++ ElseIf2.java
@@ -0,0 +1,10 @@
+public class ElseIf2{
+  public static void main(String[] args){
+    int dinheiro  = 500;
+    String sim = "SOU RICO";
+    String não = "sou pobre...";
+    String result = dinheiro >= 500 ? sim : não;
+    System.out.println(result);
+
+  }
+}
