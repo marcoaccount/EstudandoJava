c10f468d423c922fe5d0465a3e58d52317f42031
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-03-23 20:25:03 -0300

    Add files via upload

---
+++ ElseIfEX2.java
@@ -0,0 +1,11 @@
+public class ElseIfEX2 {
+  public static void main(String[] args){
+    int a1 = 30;
+    int a2 = 20;
+    if(a1 >a2){
+      System.out.println(a1+" é maior");
+    }else{
+      System.out.println(a2+" é maior");
+    }
+  }
+}
