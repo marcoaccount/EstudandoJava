22c4a15d0f96b5faa5fde038c7e4843d92477036
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-03-23 20:24:53 -0300

    Add files via upload

---
+++ ElseIfEX1.java
@@ -0,0 +1,12 @@
+public class ElseIfEX1 {
+  public static void main(String[] args){
+    int larg = 10;
+    int alt = 10;
+    if(larg == alt){
+      System.out.println("quadrardo");
+    }
+    else{
+      System.out.println("não quadrado");
+    }
+  }
+}
