0260b6e0a285b5ccfd5bf83c90633e9dd72486b6
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-04-01 20:26:17 -0300

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Switch {
+  public static void main(String[] args) {
+    System.out.println("Choice an weekday number (the first is Sunday)");
+    Scanner x = new Scanner(System.in);
+    byte WeekDay = x.nextByte();
+    switch(WeekDay){
+      case 1:
+        System.out.println("Sunday");
+        break;
+        case 2:
+        System.out.println("Monday");
+        break;
+        case 3:
+        System.out.println("Tuesday");
+        break;
+        case 4:
+        System.out.println("Wednesday");
+        break;
+        case 5:
+        System.out.println("Thursday");
+        break;
+        case 6:
+        System.out.println("Friday");
+        break;
+        case 7:
+        System.out.println("Saturday");
+        break;
+        default:
+        System.out.println("Number Invalid!");
+        break;
+    }
+  }
+}
