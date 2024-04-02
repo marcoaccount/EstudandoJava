6bde5a24aa56dd2a5b23be07c05688fedd36e82e
Marco Antônio <163692146+marcoaccount@users.noreply.github.com>
2024-04-02 20:39:23 -0300

    Add files via upload

---
+++ Repeticao.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Repeticao {
+  public static void main(String[] args) {
+    //imprima todos os numeros pares de 0 ate 1000
+    for(int pares=0;pares<=1000;pares++){
+      if (pares % 2 == 0){
+      System.out.println(pares);
+    }
+   }
+  }
+}
